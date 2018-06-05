ad4cf4e8a9d69f55076e6a771133b35529ecd618
Drevish <33568425+Drevish@users.noreply.github.com>
2018-06-05 19:10:22 +0300

    Add files via upload
    Added:
    ✔️ canvas interface
    ✔️ food and reproduction
    ✔️ user parameters setting

--- Creature.js
+++ Creature.js
@@ -1,5 +1,10 @@
 function Creature(coordinates) {
 	this.position = coordinates;
+	this.energy = 10; //start energy
+	//creature need 1 energy to make move, and gets 10 when eats food
+    this.isDead = false;
+    this.deliveredChild = false;
+
 	//array of creature nearby cells
 	this.surrounds = [[" ", " ", " "],
 					  [" ", " ", " "], 
@@ -13,7 +18,7 @@ function Creature(coordinates) {
 //creature vision
 Creature.prototype.scan = function(map) {
 	this.surrounds.forEach(function(row, i) {
-		row.forEach(function(cell, j) {
+		row.forEach(function(cell, j) {		
 			var vector = this.surroundsToVector[i][j];
 			this.surrounds[i][j] = map[this.position.x + vector[0]][this.position.y + vector[1]];
 		}.bind(this));
@@ -22,8 +27,22 @@ Creature.prototype.scan = function(map) {
 
 //move to a random empty space
 Creature.prototype.move = function() {
+	//death
+	if (this.energy == 0) {
+		this.isDead = true;
+		return;
+	}
+	
+	this.energy--;
+
+	//reproduction (division on 2 cells)
+	if (this.energy >= 30)
+		this.deliveredChild = true;
+
+
 	//cells which are free to move into
 	var emptySurrounds = [];
+	var foodCell = null;
 
 	this.surrounds.forEach(function(row, i) {
 		row.forEach(function(surround, j) {
@@ -31,13 +50,26 @@ Creature.prototype.move = function() {
 			if (surround == " ") {
 				emptySurrounds.push([i, j]);
 			}
+
+			//food found, instant eat
+			if (surround == "*") {
+				foodCell = [i,j];
+			}
+
 		}.bind(this));
 	}.bind(this));
+
 	//deadlock, mo move
 	if (emptySurrounds.length == 0) return;
 
-	var moveNumber = Math.round( Math.random() * (emptySurrounds.length - 1));
-	var vector = this.surroundsToVector[emptySurrounds[moveNumber][0]][emptySurrounds[moveNumber][1]];	
+	if (foodCell) {
+		var newCell = foodCell;
+		this.energy += 10;
+	}
+	else
+		var newCell = emptySurrounds[Math.floor(Math.random() * emptySurrounds.length)];
+
+	var vector = this.surroundsToVector[newCell[0]][newCell[1]];	
 	this.position.x += vector[0];
 	this.position.y += vector[1];
 }
--- Game.js
+++ Game.js
@@ -1,8 +1,11 @@
-function Game(gameMap, outputContext, delay) {
+function Game(gameMap, outputContext, delay, foodCountSpawnPerTurn, cellSize, creaturesCount) {
     this.map = gameMap;				
     this.output = outputContext;
     this.delay = delay;
     this.creatures = [];
+    this.foodCount = foodCountSpawnPerTurn;
+    this.cellSize = cellSize;
+    this.creaturesCount = creaturesCount;
 
     this.map.forEach(function(row, i) {
 		row.forEach(function(symbol, j) {
@@ -12,6 +15,10 @@ function Game(gameMap, outputContext, delay) {
 			}
 		}.bind(this));
 	}.bind(this));
+
+    //canvas initializing
+    this.output.setAttribute("width",  map.length * this.cellSize);
+    this.output.setAttribute("height",  map[0].length * this.cellSize);
 }
 
 //refreshing map when position of creatures has changed
@@ -32,10 +39,29 @@ Game.prototype.refreshMap = function() {
 
 //printing an image or text of a map to browser
 Game.prototype.print = function() {
-	this.output.innerHTML = "";
-	this.map.forEach(function(row) {
-		row.forEach(function(symbol) {
-			this.output.innerHTML += symbol;
+	this.output.width = this.output.width;
+	var context = this.output.getContext("2d");
+	
+	this.map.forEach(function(row, i) {
+		row.forEach(function(symbol, j) {
+			if (symbol == "#") {
+				context.fillStyle="#000000";
+				context.fillRect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize);
+			}
+
+			if (symbol == "0") {
+				context.beginPath();
+				context.arc(i * this.cellSize + this.cellSize / 2, j * this.cellSize + this.cellSize / 2, this.cellSize / 2, 0,  Math.PI * 2);
+				context.fillStyle="#00FF00";
+				context.fill();
+			}
+
+			if (symbol == "*") {
+				context.beginPath();
+				context.arc(i * this.cellSize + this.cellSize / 2, j * this.cellSize + this.cellSize / 2, this.cellSize / 2, 0,  Math.PI * 2);
+				context.fillStyle="#FFFF2A";
+				context.fill();
+			}
 		}.bind(this));
 		this.output.innerHTML += "<br>";
 	}.bind(this));
@@ -43,6 +69,7 @@ Game.prototype.print = function() {
 
 //starting game loop
 Game.prototype.start = function() {
+	console.log(this);
 	setInterval(function() {
 		this.makeMove();
 		this.refreshMap();
@@ -50,11 +77,47 @@ Game.prototype.start = function() {
 	}.bind(this), this.delay);
 }
 
+Game.prototype.purgeCell = function(pos){
+	this.map[pos.x][pos.y] = " ";
+}
+
+Game.prototype.spawnFood = function() {
+	var emptyCells = [];
+
+	this.map.forEach(function(row, i) {
+		row.forEach(function(cell, j) {
+			if (cell == " ")
+				emptyCells.push([i, j]);
+		}.bind(this));
+	}.bind(this));
+
+	//deadlock
+	if (emptyCells.length == 0) return;
+
+	for(var i = 0; i < this.foodCount; i++) {
+		var moveNumber = Math.floor( Math.random() * emptyCells.length);
+		var cell = emptyCells[moveNumber];
+		this.map[cell[0]][cell[1]] = "*";
+	}
+}
+
 //calculation of every creature move
 Game.prototype.makeMove = function() {
-	this.creatures.forEach(function(creature) {
+	this.creatures.forEach(function(creature, counter) {
 		//we have to give map to our creature in order to it could scan nearby cells
 		creature.scan(this.map);
 		creature.move();
-	});
+
+		if (creature.isDead) {
+			this.purgeCell(creature.position);
+			this.creatures = arrayDeleteElement(this.creatures, counter);
+		}
+		if(creature.deliveredChild) {
+			creature.deliveredChild = false;
+			creature.energy -= 20;
+			this.creatures.push(new Creature(new Position(creature.position.x, creature.position.y)));
+		}
+	}.bind(this));
+
+	this.spawnFood();
 }
